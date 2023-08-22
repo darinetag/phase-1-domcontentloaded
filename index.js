ddb05eb6800549dbf678a7a34ba7425af874aac5
darinetag <tagdarinefatma@gmail.com>
2023-08-22 21:14:02 +0100

    assignmetn done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+ 
+  const paragraph = document.querySelector('p');
+
+  paragraph.textContent = "This is really cool!";
+  
+});
+
